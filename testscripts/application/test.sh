25a0aaaa99054fd52d70a99a6c3e67415fefd417
Bjarte Stien Karlsen <Bjarte.Karlsen@skatteetaten.no>
2017-08-03 13:47:08 +0200

    added note on testing

--- testscripts/application/test.sh
+++ testscripts/application/test.sh
@@ -3,4 +3,7 @@ oc whoami -t || (echo "You must be logged into openshift" && exit 1)
 
 token=$(oc whoami -t)
 
+
+//her må man port forwarde port 3000 fra paas-utv/refapp til lokal maskin
+
 http --timeout 300 GET :8080/aurora/application/namespace/paas-utv/application/refapp Authorization:"bearer $token"
